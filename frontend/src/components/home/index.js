542093a5c1351c431bb6675e857ea75cfd799193
victor212luan <streamyodin@gmail.com>
2020-03-16 23:38:43 -0300

    add main-cards.css to main page

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "../card";
 
+import '../../assets/css/components/main-cards.css';
+
 function Home(){
   return(
-    <section>
+    <section className="main-cards">
       <Card arquivo="" titulo="Cadastrar Container" link="/container"/>
-      <Card arquivo="" titulo="Cadastrar Movimentações" link="/movimentacao"/>
+      <Card arquivo="" titulo="Cadastrar Movimentações" link="/movimentacoes"/>
       <Card arquivo="" titulo="Pesquisa" link="/pesquisa"/>
       <Card arquivo="" titulo="Relatório" link="/relatorio"/>
     </section>
